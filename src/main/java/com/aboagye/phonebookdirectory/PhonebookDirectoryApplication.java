673bd74df8904ea6e16dfef853668718744bdfc4
jessebrite <aboagye@protonmail.com>
2021-05-11 16:33:56 +0100

    initial commit

---
+++ src/main/java/com/aboagye/phonebookdirectory/PhonebookDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.aboagye.phonebookdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aboagye/phonebookdirectory/PhonebookDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboagye.phonebookdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
